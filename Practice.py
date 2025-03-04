65bb024794747e7cb83f4edc20c6e925e58ca201
Hrishikesh579 <hrishikeshwarpe1@gmail.com>
2025-03-05 00:48:09 +0530

    Initial Commit

---
+++ Practice.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+post = [
+    {
+        'author' : 'Jon',
+        'title': 'Vlog Post1',
+        'Content': 'First Post Content',
+        'date_added' :'26_8_20030'
+    },
+    {
+        'author' : 'Vicky',
+        'title' : 'Vlog Post2',
+        'Content' : 'Second Post Content',
+        'date_added' : '29_5_2007'
+    }
+    ]
+@app.route('/')
+def hello_world():
+    return render_template('primary.html',post = post)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title = 'About')
+if __name__ == "__main__":
+    app.run(debug=True)
